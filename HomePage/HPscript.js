f0c56cc8b92f71816ef6fd76db29a735f1179e96
Zephaniah Gustafson <zevangus@gmail.com>
2024-06-06 09:46:06 -0500

    don't remeber what I did, its been a bit

--- HomePage/HPscript.js
+++ HomePage/HPscript.js
@@ -3,12 +3,14 @@ const subHeader = document.querySelector('h2');
 const buttonOptions = document.querySelector('.options-Container');
 
 const animatedItems = [header, subHeader, buttonOptions];
+let transitionFinished = false;
 
 function transition(items) {
     items.forEach((item, index) => {
         setTimeout(() => {
             item.style.opacity = 1;
         }, (index + 1) * 1000); 
+        transitionFinished = true;
     });
 }
 
@@ -17,8 +19,18 @@ document.addEventListener("DOMContentLoaded", () => transition(animatedItems));
 const fancyText = document.getElementById('title-text');
 const button1 = document.getElementById('button1');
 const button2 = document.getElementById('button2');
-addEventListener('transitionend', () => {
+document.addEventListener("mousemove", () => {
+    if(transitionFinished) {
     fancyText.classList.add('text');
     button1.classList.add('text');
     button2.classList.add('text');
+    }
 });
+
+
+document.getElementById("button1").onclick = () => {
+    window.location.href = 'RatingsList/ratingsPage.html';
+}
+document.getElementById("button1").onclick = () => {
+    window.location.href = 'tbrList/tbrPage.html';
+}
